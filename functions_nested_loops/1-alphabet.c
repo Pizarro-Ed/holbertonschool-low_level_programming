ff92ca4c4fa482abc4828e8094719e627f3d8eba
Pizarro-Ed <ed.pizarro23@outlook.com>
2023-10-17 20:55:18 -0400

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
 /**
- * main - print alphabet in lowercase
+ * print_alphabet - wirte the lower-case alphabet on a line.
  *
- * Return: Always 0.
+ *
+ *
  */
-int main(void)
+void print_alphabet(void)
 {
-    print_alphabet('a-z');
-    return (0);
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
+
